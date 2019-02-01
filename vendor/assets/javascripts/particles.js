b4fe986fe862464455d5ea6ebe04fc83bba1c532
Zakui <zakuijules@gmail.com>
2019-02-01 13:40:44 +0000

    remove initializer conf

--- vendor/assets/javascripts/particles.js
+++ vendor/assets/javascripts/particles.js
@@ -88,7 +88,7 @@ var pJS = function(tag_id, params){
       array: []
     },
     interactivity: {
-      detect_on: 'canvas',
+      detect_on: '.particles-js-canvas-el',
       events: {
         onhover: {
           enable: true,
@@ -1489,33 +1489,9 @@ window.particlesJS = function(tag_id, params){
     tag_id = 'particles-js';
   }
 
-  /* pJS elements */
-  var pJS_tag = document.getElementById(tag_id),
-      pJS_canvas_class = 'particles-js-canvas-el',
-      exist_canvas = pJS_tag.getElementsByClassName(pJS_canvas_class);
-
-  /* remove canvas if exists into the pJS target tag */
-  if(exist_canvas.length){
-    while(exist_canvas.length > 0){
-      pJS_tag.removeChild(exist_canvas[0]);
-    }
-  }
-
-  /* create canvas element */
-  var canvas_el = document.createElement('canvas');
-  canvas_el.className = pJS_canvas_class;
-
-  /* set size canvas */
-  canvas_el.style.width = "100%";
-  canvas_el.style.height = "100%";
-
-  /* append canvas */
-  var canvas = document.getElementById(tag_id).appendChild(canvas_el);
 
   /* launch particle.js */
-  if(canvas != null){
-    pJSDom.push(new pJS(tag_id, params));
-  }
+  pJSDom.push(new pJS(tag_id, params));
 
 };
 
